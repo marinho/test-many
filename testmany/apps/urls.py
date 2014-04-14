bb117b6c7611e1552102884d36f7cef2ff6bbaf9
Marinho Brandao <marinho@gmail.com>
2014-04-14 15:13:27 +0200

    Supporting pushes

--- testmany/apps/urls.py
+++ testmany/apps/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('apps.views',
     url(r'^$', "index"),
     url(r'^projects\.json$', "projects_json"),
+    url(r'^push-info$', "push_info"),
 )
 
--- testmany/apps/views.py
+++ testmany/apps/views.py
@@ -1,8 +1,10 @@
 import json
 from django.shortcuts import render
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse
 from django.utils import timesince
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.timezone import now
 
 from apps.models import Project
 
@@ -28,3 +30,22 @@ def format_project(project):
         "coverage": project.last_coverage(),
         "status": project.last_status(),
     }
+
+
+@csrf_exempt
+def push_info(request):
+    package = request.POST["package"]
+    version = request.POST["version"]
+    coverage = request.POST["coverage"]
+    status = request.POST["status"]
+
+    project = get_object_or_404(Project, name=package)
+
+    project.history.create(
+        finished = now(),
+        version = version,
+        coverage = int(coverage),
+        status = status,
+        )
+
+    return HttpResponse("OK", content_type="text/plain")
